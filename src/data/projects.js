db9b7a21e9a41659ff76d0198777d6d95eb98f35
devenvoy <iambeast0069@gmail.com>
2025-07-03 14:59:36 +0530

    add dialer and testers images; update project descriptions and technologies

--- src/data/projects.js
+++ src/data/projects.js
@@ -14,17 +14,40 @@ const projects = [
     ],
   },
    {
-    img: "./gradnet.png",
-    name: "GradNet Graduate's Network",
-    demo: "./gradnet.png",
-    description:
-      "GradNet is a platform that connects recent graduates with employers. It allows employers to post jobs and search for candidates. It also allows candidates to search for jobs and apply to them.",
+    img: "./dialer.png",
+    name: "IPhone's Dialer",
+    demo: "https://play.google.com/store/apps/details?id=com.contact.idialer.phonecaller",
+description:
+      `Caller - Phone Contacts Dialer: Enhance Your Calling Experience
+
+Transform your phone calling with Phone Dialer, look like iPhone style dialer, the ultimate Contacts & Phone Dialer app that combines style, efficiency, and advanced features for a seamless communication experience.`,
     technologies: [
       "Android",
-      "IOS",
       "Kotlin",
       "Compose",
-      "Compose multiplatform",
+      "MVVM",
+      "Cupportino Design",
+      "Hilt",
+      "Coroutines",
+      "Flow",
+      "Room",
+    ],
+  },
+   {
+    img: "./testers.png",
+    name: "20 Testers Community",
+    demo: "https://play.google.com/store/apps/details?id=com.testingcommunity.twentytesters",
+description:
+`20 Testers Community is the perfect platform to connect app developers and testers, helping you get valuable feedback for your app. Secure 20 testers for 14 days, completely free, and ensure your app is ready for the Play Store.`,
+    technologies: [
+      "Android",
+      "Kotlin",
+      "Compose",
+      "MVVM",
+      "Hilt",
+      "Coroutines",
+      "Flow",
+      "Retrofit","API","Firebase","Push Notifications",
     ],
   },
 ];
