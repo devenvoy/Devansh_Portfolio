4db62e30c33264b15a6dd5a36ca1e366474528ad
devenvoy <iambeast0069@gmail.com>
2025-09-29 18:05:33 +0530

    next.js release fixes

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,17 @@
-import type { NextConfig } from 'next';
+const isGithubPages = process.env.VERCEL_ENV !== "production"; 
+const repoName = "/Devansh_Portfolio";
 
-const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+const nextConfig = {
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**',
-      },
+      { protocol: "https", hostname: "picsum.photos", pathname: "/**" },
+      { protocol: "https", hostname: "res.cloudinary.com", pathname: "/**" },
     ],
   },
+  basePath: repoName,
+  assetPrefix: repoName,
 };
 
 export default nextConfig;
