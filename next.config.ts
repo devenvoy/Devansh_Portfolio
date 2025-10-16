5bdac1d5415a1b1e257e936005eee507436057f2
devenvoy <iambeast0069@gmail.com>
2025-10-16 12:11:51 +0530

    chore: clean up next.config.ts by removing unused configurations

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,14 @@
-const repoName = "";
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  unoptimized: true,
-  trailingSlash: true,
   reactStrictMode: true,
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
-   experimental: {
-    legacyBrowsers: false, // 🔥 Drop IE11 & legacy polyfills
-  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "picsum.photos", pathname: "/**" },
       { protocol: "https", hostname: "res.cloudinary.com", pathname: "/**" },
     ],
   },
-  output: "export",
-  env: {
-    NEXT_PUBLIC_BASE_PATH: repoName,
-  },
-  basePath: repoName,
-  assetPrefix: repoName,
 };
 
 module.exports = nextConfig;
-export default nextConfig;
